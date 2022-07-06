9af75c33f7223e6525d7c50931ee67c61921cc1e
JediSim <simpiccc@gmail.com>
2022-07-06 15:58:22 +0200

    install quasar

---
+++ src/components/CounterItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>bonjour</div>
+  <p>{{ count }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">decrement</button>
+</template>
+
+<script>
+export default {
+  name: "CounterItem",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
+</script>
---
+++ src/components/TicTacToeGrid.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+    <ul v-for="(row, index) in grid" :key="index">
+      <ul v-for="(cell, i) in row" :key="i">
+        <li>{{ cell }}</li>
+      </ul>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "TicTacToeGrid",
+  data() {
+    return {
+      grid: [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
+      player: 1,
+      winner: 0,
+    };
+  },
+};
+</script>
+<style lang="" scoped></style>
